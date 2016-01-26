0907221ac871bc715d5be166829458fb6fd77f54
Sean Baillargeon <seanpbaillargeon@gmail.com>
2016-01-26 16:09:34 -0700

    booyah

--- java.js
+++ java.js
@@ -3,23 +3,42 @@ angular.module('moduleOne', [])
 angular.module('moduleOne')
 	.controller('controllerOne', ["$scope", function($scope){
 
-		$scope.spot = {x:0, y:0}
+		$scope.spot = {}
 		$scope.show = false
+		$scope.textBox = false 
 
 		$scope.locations = []
 		console.log($scope.spot)
 		console.log($scope.locations)
 
-		$scope.click = function($event){
-			console.log(event);
-			$scope.spot.x = event.offsetX;
-			$scope.spot.y = event.offsetY;
-			$scope.show = true;
-			console.log($scope.spot.x, $scope.spot.y);
-			$scope.locations.push($scope.spot);
 
+
+		$scope.click = function($event, $index){
+			console.log(event);
+			$scope.spot = {    
+				x:event.offsetX-5,
+				y:event.offsetY-5,
+				note: prompt("Enter a note!")
+			}
+			$scope.locations.push($scope.spot);
+			$scope.show = true;
+			console.log($scope.locations)
+			console.log($scope.spot.x, $scope.spot.y);
 		} 
 
-			
+		$scope.removeSpot = function($index) {
+			$scope.locations.splice($index, 1)
+			console.log($scope.locations)
+		}	
 
-	}])
+		$scope.enter = function($index) {
+			$scope.textBox = true
+			console.log("poop")
+		}
+
+		$scope.leave = function($index) {
+			$scope.textBox = false
+			console.log("Turd")
+		}
+
+	}])		
